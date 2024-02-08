dfa9311d1b0693783fbed8174d05b66697c5a16a
ismet01a <159360894+ismet01a@users.noreply.github.com>
2024-02-08 09:39:30 +0300

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_kaydet = new System.Windows.Forms.Button();
+            this.btn_güncelle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.dgw = new System.Windows.Forms.DataGridView();
+            this.txt_ad = new System.Windows.Forms.TextBox();
+            this.txt_borc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_kaydet
+            // 
+            this.btn_kaydet.Location = new System.Drawing.Point(49, 341);
+            this.btn_kaydet.Name = "btn_kaydet";
+            this.btn_kaydet.Size = new System.Drawing.Size(75, 23);
+            this.btn_kaydet.TabIndex = 0;
+            this.btn_kaydet.Text = "kaydet";
+            this.btn_kaydet.UseVisualStyleBackColor = true;
+            // 
+            // btn_güncelle
+            // 
+            this.btn_güncelle.Location = new System.Drawing.Point(49, 313);
+            this.btn_güncelle.Name = "btn_güncelle";
+            this.btn_güncelle.Size = new System.Drawing.Size(84, 22);
+            this.btn_güncelle.TabIndex = 1;
+            this.btn_güncelle.Text = "güncelle";
+            this.btn_güncelle.UseVisualStyleBackColor = true;
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Location = new System.Drawing.Point(343, 313);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(75, 23);
+            this.btn_sil.TabIndex = 2;
+            this.btn_sil.Text = "sil";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(343, 342);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(75, 23);
+            this.btn_ekle.TabIndex = 3;
+            this.btn_ekle.Text = "ekle";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            // 
+            // dgw
+            // 
+            this.dgw.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw.Location = new System.Drawing.Point(49, 70);
+            this.dgw.Name = "dgw";
+            this.dgw.Size = new System.Drawing.Size(399, 200);
+            this.dgw.TabIndex = 4;
+            // 
+            // txt_ad
+            // 
+            this.txt_ad.Location = new System.Drawing.Point(49, 287);
+            this.txt_ad.Name = "txt_ad";
+            this.txt_ad.Size = new System.Drawing.Size(100, 20);
+            this.txt_ad.TabIndex = 5;
+            // 
+            // txt_borc
+            // 
+            this.txt_borc.Location = new System.Drawing.Point(343, 287);
+            this.txt_borc.Name = "txt_borc";
+            this.txt_borc.Size = new System.Drawing.Size(100, 20);
+            this.txt_borc.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "borç";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_borc);
+            this.Controls.Add(this.txt_ad);
+            this.Controls.Add(this.dgw);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_güncelle);
+            this.Controls.Add(this.btn_kaydet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgw)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_kaydet;
+        private System.Windows.Forms.Button btn_güncelle;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.DataGridView dgw;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.TextBox txt_borc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace WindowsFormsApp3
+{
+        public partial class Form1 : Form
+        {
+            OleDbConnection connection = new OleDbConnection();
+            public Form1()
+            {
+                InitializeComponent();
+                connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\borc_veritabani.accdb;Persist Security Info=False;";
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                VerileriGetir();
+            }
+
+            private void VerileriGetir()
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = connection;
+                command.CommandText = "select * from borclar";
+                OleDbDataAdapter da = new OleDbDataAdapter(command);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgw.DataSource = dt;
+                connection.Close();
+            }
+
+            private void btn_ekle_Click(object sender, EventArgs e)
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = connection;
+                command.CommandText = "insert into borclar (Ad, Borc) values('" + txt_ad.Text + "', '" + txt_borc.Text + "')";
+                command.ExecuteNonQuery();
+                connection.Close();
+                VerileriGetir();
+            }
+
+            private void btn_sil_Click(object sender, EventArgs e)
+            {
+                if (dgw.SelectedRows.Count > 0)
+                {
+                    connection.Open();
+                    OleDbCommand command = new OleDbCommand();
+                    command.Connection = connection;
+                    command.CommandText = "delete from borclar where ID=" + dgw.SelectedRows[0].Cells["ID"].Value.ToString() + "";
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    VerileriGetir();
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen bir satır seçin.");
+                }
+            }
+
+            private void btn_guncelle_Click(object sender, EventArgs e)
+            {
+                if (dgw.SelectedRows.Count > 0)
+                {
+                    connection.Open();
+                    OleDbCommand command = new OleDbCommand();
+                    command.Connection = connection;
+                    command.CommandText = "update borclar set Ad='" + txt_ad.Text + "', Borc='" + txt_borc.Text + "' where ID=" + dgw.SelectedRows[0].Cells["ID"].Value.ToString() + "";
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    VerileriGetir();
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen bir satır seçin.");
+                }
+            }
+
+            private void btn_kaydet_Click(object sender, EventArgs e)
+            {
+                // Değişiklikleri veritabanına kaydetmek için buraya gerekli kodu ekleyebilirsiniz.
+            }
+        }
+    }
+
